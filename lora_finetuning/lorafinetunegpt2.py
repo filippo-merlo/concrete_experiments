e012195e14f9f420dfadeb0c8e2d8faa0ca5c7fd
Filippo <filippo99merlo@gmail.com>
2024-10-13 18:26:11 +0200

    rth

---
+++ lora_finetuning/lorafinetunegpt2.py
@@ -0,0 +1,238 @@
+# Fine-Tuning GPT-2 on Encrypted Data with LoRA and Concrete-ML
+
+# Import necessary libraries
+import math
+import shutil
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import torch
+from peft import LoraConfig, TaskType, get_peft_model
+from tqdm import tqdm
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    TextDataset,
+    Trainer,
+    TrainingArguments,
+)
+from utils_lora import generate_text, print_weights_and_size
+
+from concrete.ml.torch.hybrid_model import HybridFHEModel
+from concrete.ml.torch.lora import LoraTraining, get_remote_names
+
+# Set random seed for reproducibility
+SEED = 0
+torch.manual_seed(SEED)
+torch.use_deterministic_algorithms(True)
+# Load pre-trained GPT-2 model and tokenizer
+cache_dir = "/mnt/cimec-storage6/users/filippo.merlo/concrete_experiments/cache"
+model_cache_dir = "/mnt/cimec-storage6/users/filippo.merlo/concrete_experiments/models"
+model_name = "gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=model_cache_dir)
+
+# Ensure tokenizer has a pad token
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
+# Freeze model weights
+for param in model.parameters():
+    param.requires_grad = False
+# Example usage of the pre-trained model
+torch.manual_seed(SEED)
+prompt = "What is FHE?"
+generated_text = generate_text(prompt, model, tokenizer)
+print(generated_text)
+# Configure LoRA
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    fan_in_fan_out=True,
+)
+
+# Apply LoRA to the model
+peft_model = get_peft_model(model, peft_config)
+# Set up LoRA training
+lora_training = LoraTraining(peft_model)
+# Prepare dataset for fine-tuning
+BLOCK_SIZE = 128
+
+train_dataset = TextDataset(
+    tokenizer=tokenizer,
+    file_path="data_finetune/what_is_fhe.txt",
+    block_size=BLOCK_SIZE,
+    cache_dir=cache_dir,
+)
+# Set up data collator for language modeling
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+# Define training arguments
+EPOCHS = 50
+PER_DEVICE_TRAIN_BATCH_SIZE = 4
+
+training_args = TrainingArguments(
+    output_dir="/mnt/cimec-storage6/users/filippo.merlo/concrete_experiments/models/checkpoints",
+    num_train_epochs=EPOCHS,
+    per_device_train_batch_size=8,
+    gradient_accumulation_steps=2,
+    save_total_limit=1,
+    use_cpu=True,
+    learning_rate=5e-3,
+    logging_strategy="epoch",
+    optim="adamw_torch",
+    seed=SEED,
+    data_seed=SEED,
+    weight_decay=0.0,
+    warmup_steps=0,
+    max_grad_norm=1.0,
+)
+# Initialize Trainer
+trainer = Trainer(
+    model=peft_model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_dataset,
+)
+
+# Prepare for training
+train_dataloader = trainer.get_train_dataloader()
+
+len_dataloader = len(train_dataloader)
+num_update_steps_per_epoch = len_dataloader // training_args.gradient_accumulation_steps
+num_update_steps_per_epoch = max(num_update_steps_per_epoch, 1)
+max_steps = math.ceil(training_args.num_train_epochs * num_update_steps_per_epoch)
+
+trainer.create_optimizer_and_scheduler(num_training_steps=max_steps)
+
+lora_training.update_training_parameters(
+    trainer.optimizer, trainer.lr_scheduler, None, training_args
+)
+# Get the names of the remote modules (layers to be converted to FHE)
+remote_names = get_remote_names(lora_training, include_embedding_layers=False)
+# Create the HybridFHEModel with the specified remote modules
+hybrid_model = HybridFHEModel(lora_training, module_names=remote_names)
+# Prepare input data for calibration
+input_tensor = torch.randint(0, 2, (PER_DEVICE_TRAIN_BATCH_SIZE, BLOCK_SIZE)) * (
+    tokenizer.vocab_size - 1
+)
+label_tensor = torch.randint(0, 2, (PER_DEVICE_TRAIN_BATCH_SIZE, BLOCK_SIZE)) * (
+    tokenizer.vocab_size - 1
+)
+
+inputset = (input_tensor, label_tensor)
+# Calibrate and compile the model
+hybrid_model.model.toggle_calibrate(enable=True)
+hybrid_model.compile_model(inputset, n_bits=16)
+hybrid_model.model.toggle_calibrate(enable=False)
+def train_custom_model(
+    hybrid_model, train_dataloader, training_args, fhe="disable"
+):  # pylint: disable=too-many-locals
+    device = "cpu"
+    hybrid_model.model.to(device)
+
+    # Training loop
+    hybrid_model.model.inference_model.train()
+    hybrid_model.model.run_optimizer = True
+    total_epochs = int(training_args.num_train_epochs)
+    epoch_pbar = tqdm(total=total_epochs, desc="Training Progress", position=0)
+
+    total_batched_samples = 0
+    epoch_losses = []  # List to store the loss for each epoch
+
+    for epoch in range(total_epochs):
+        total_loss = 0
+        grad_norms = []
+
+        for _, batch in enumerate(train_dataloader):
+            total_batched_samples += 1
+
+            batch = {k: v.to(device) for k, v in batch.items()}
+
+            loss, grad_norm = hybrid_model((batch["input_ids"], batch["labels"]), fhe=fhe)
+
+            total_loss += loss.item()
+
+            if grad_norm is not None:
+                grad_norms.append(grad_norm)
+
+        # Get current learning rate
+        current_lr = hybrid_model.model.lr_scheduler.get_last_lr()[0]
+
+        # Get last grad norm
+        current_grad_norm = grad_norms[-1] if grad_norms else None
+
+        # Store the total loss for this epoch
+        epoch_losses.append(total_loss)
+
+        # Log epoch results
+        print(
+            f"Epoch {epoch + 1}/{training_args.num_train_epochs}, "
+            f"Loss: {total_loss:.4f}, grad norm: {current_grad_norm}, lr: {current_lr}"
+        )
+
+        epoch_pbar.update(1)
+
+    # Save model checkpoint
+    if training_args.output_dir is not None:
+        save_path = f"{training_args.output_dir}/checkpoint-{epoch + 1}"
+        hybrid_model.model.inference_model.save_pretrained(save_path)
+
+    epoch_pbar.close()
+
+    # Plot the loss evolution
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, total_epochs + 1), epoch_losses, marker="o")
+    plt.title("Loss Evolution During Training")
+    plt.xlabel("Epoch")
+    plt.ylabel("Total Loss")
+    plt.grid(True)
+    plt.show()
+# Avoid parallelism error from HuggingFace during training
+tokenizer.parallelism = False
+
+# Train the model using FHE simulation
+train_custom_model(hybrid_model, train_dataloader, training_args, fhe="simulate")
+# Get the fine-tuned model
+fine_tuned_model = hybrid_model.model.inference_model
+
+# Set FHE mode to disable for text generation
+hybrid_model.set_fhe_mode("disable")
+# Inference using the fine-tuned model with LoRA weights
+# Seed for reproducibility
+torch.manual_seed(SEED)
+
+prompt = "What is FHE?"
+generated_text = generate_text(prompt, fine_tuned_model, tokenizer)
+print(generated_text)
+# Original inference without LoRA weights
+# Seed for reproducibility
+torch.manual_seed(SEED)
+
+peft_model.disable_adapter_layers()
+
+prompt = "What is FHE?"
+generated_text = generate_text(prompt, fine_tuned_model, tokenizer)
+print(generated_text)
+peft_model.enable_adapter_layers()
+
+# Print weights and model size
+total_weights_size = print_weights_and_size(hybrid_model.model)
+# Save the model
+path = Path("/mnt/cimec-storage6/users/filippo.merlo/concrete_experiments/models/gpt2_lora_finetuned")
+path.mkdir(parents=True, exist_ok=True)
+
+if path.is_dir() and any(path.iterdir()):
+    shutil.rmtree(path)
+
+hybrid_model.save_and_clear_private_info(path)
+# Print weights and size after saving
+total_weights_size_private = print_weights_and_size(hybrid_model.model)
+# Calculate and print the percentage of weights removed
+percentage_removed = (total_weights_size - total_weights_size_private) / total_weights_size * 100
+print(f"Total weights removed: {percentage_removed:.2f} %")
+# Note: Around 95% of the remaining weights are from the embedding layers (wpe and wte)
+# as well as the final lm_head layer.
