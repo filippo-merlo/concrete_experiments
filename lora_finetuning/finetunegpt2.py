0b56daae7a940453da8b779af5eebf486a8d2bf3
Filippo <filippo99merlo@gmail.com>
2024-10-13 17:44:55 +0200

    erg

--- lora_finetuning/finetunegpt2.py
+++ lora_finetuning/finetunegpt2.py
@@ -216,12 +216,12 @@ attention_mask = torch.ones((PER_DEVICE_TRAIN_BATCH_SIZE, BLOCK_SIZE))
 inputset = (input_tensor, label_tensor, attention_mask)
 # Calibrate and compile the model
 lora_training.toggle_calibrate(enable=True)
-hybrid_model.compile_model(inputset, n_bits=16, device="cuda")
+hybrid_model.compile_model(inputset, n_bits=16, device="cpu")
 lora_training.toggle_calibrate(enable=False)
 def train_custom_model(
     hybrid_model, train_dataloader, training_args, tokenizer, fhe="disable"
 ):  # pylint: disable=too-many-locals
-    device = "cuda"
+    device = "cpu"
     hybrid_model.model.to(device)
 
     # Training loop
